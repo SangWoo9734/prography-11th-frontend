ae46494ea996ba0127b6f5c11946b56db288d619
SangWoo Park <psu8430@gmail.com>
2026-02-24 16:49:01 +0900

    refactor(layout): update root layout with sidebar and query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import SideBar from "./components/sidebar";
+import { QueryProvider } from "./components/QueryProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="h-screen w-screen">
+        <div className="h-full flex">
+          <SideBar />
+          <QueryProvider>{children}</QueryProvider>
+        </div>
       </body>
     </html>
   );
