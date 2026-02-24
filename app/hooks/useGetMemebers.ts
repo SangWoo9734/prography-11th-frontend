a9da531a796ccaecbef7a5462bbd186d5f7e1c29
SangWoo Park <psu8430@gmail.com>
2026-02-24 16:49:17 +0900

    feat(user): add useGetMembers hook

---
+++ app/hooks/useGetMemebers.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { GetMemberParams, getMembers } from "../api/user";
+
+export function useGetMembers(
+  params: GetMemberParams,
+  options?: { enabled?: boolean },
+) {
+  const { data, isInitialLoading } = useQuery({
+    queryKey: ["members", params.page],
+    queryFn: () => getMembers(params),
+    enabled: options?.enabled,
+  });
+
+  return {
+    userList: data?.content ?? [],
+    totalPage: data?.totalPages ?? 0,
+    isLoading: isInitialLoading,
+  };
+}
