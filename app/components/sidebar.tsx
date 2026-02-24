78a21ce446a19422acbd6849c525e89ba57def2a
SangWoo Park <psu8430@gmail.com>
2026-02-24 16:48:29 +0900

    feat(components): add sidebar navigation component

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../assets/logo.png";
+
+export default function SideBar() {
+  return (
+    <div className="h-full w-40 flex flex-col p-3 border-r-2">
+      <Image src={logo} alt="logo" width={80} height={30} className="mb-4" />
+      <Link href="/user">회원 관리</Link>
+      <Link href="/attendance">출결 관리</Link>
+      <Link href="/session">세션 관리</Link>
+    </div>
+  );
+}
