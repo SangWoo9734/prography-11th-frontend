60ba8431fdbdb39910aab66f78cf01e7ca9a54ed
SangWoo Park <psu8430@gmail.com>
2026-02-26 19:48:48 +0900

    feat(sidebar): make logo link to /user

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,7 +5,9 @@ import logo from "../assets/logo.png";
 export default function SideBar() {
   return (
     <div className="h-full w-40 flex flex-col p-3 border-r-2">
-      <Image src={logo} alt="logo" width={80} height={30} className="mb-4" />
+      <Link href="/user">
+        <Image src={logo} alt="logo" width={80} height={30} className="mb-4" />
+      </Link>
       <Link href="/user">회원 관리</Link>
       <Link href="/attendance">출결 관리</Link>
       <Link href="/session">세션 관리</Link>
